9a57b3f94572c2a238cd1c96f2cad96f2cb97378
Luiza Henry <luizahenry@yahoo.com>
2017-08-19 22:20:22 +0000

    adding content

--- hello.rb
+++ hello.rb
@@ -9,5 +9,11 @@ puts "testing this thing"
           "a slice of stale wedding cake",
           "a medium-sized island off the coast of Sumatra",
           "a visual prosthesis",
+          "no idea what I am doing",
+          "almost there",
+          "once again",
+          "last chance"
          ]
   puts "All I got for valentine's day was #{gift.sample}."
+  
+  
