a68332763d54bfbd1fc28027b1147cb7b0cea996
Luiza Henry <luizahenry@yahoo.com>
2017-08-19 21:10:46 +0000

    new gift

--- hello.rb
+++ hello.rb
@@ -1,2 +1,13 @@
 puts "hello world" 
 puts "testing this thing"
+
+ gift = [
+          "half of a leftover burrito",
+          "a day-old cup of coffee",
+          "a tesla",
+          "a life-size quadrocopter",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
